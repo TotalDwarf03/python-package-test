be20c85770f177c627b71e8289ef054bb2ffe26f
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2024-07-05 09:29:09 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='my-package',
+    name='my_package',
     version='0.0.1',
     description='My package from github repo',
     url='git@github.com:TotalDwarf03/python-package-test.git',

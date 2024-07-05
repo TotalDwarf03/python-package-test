ce6d28cf3e3b805b71b59edcdaddff788ca2f048
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2024-07-05 09:18:07 +0100

    setup structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="my_package",
+    version="0.1",
+    description="A cool test package",
+    packages=["test_module", "additional_module"]
+)
---
+++ test-module/__init__.py
@@ -0,0 +1,5 @@
+def test_method():
+    print("Test method called")
+
+def test_method_params(param: str):
+    print(f"Test method called with param: {param}")
